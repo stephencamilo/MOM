c8afada6bd07326d938b2742f9604088f78c53b6
Stephen Eduardo Camilo <stephen.camilo@live.com>
2021-02-21 14:59:17 -0300

    WP automated login

---
+++ TestWPLogin.php
@@ -0,0 +1,26 @@
+<?php
+require __DIR__ . "/vendor/autoload.php";
+//  php ./TestWPInstall.php "url" "projectname" "pass"
+
+use Symfony\Component\Panther\Client;
+
+$client = Client::createChromeClient();
+
+$client->request("GET", $argv[1]);
+
+var_dump($argv[3]);
+
+try {
+    $crawler = $client->waitForVisibility("#wp-submit");
+    $client->submitForm(
+        "log In",
+        [
+            "user_login" => $argv[2],
+            "user_pass" => $argv[3],
+        ]
+    );
+} catch (Exception $e) {
+    var_dump($e);
+}
+
+$client->takeScreenshot("screen.png");
